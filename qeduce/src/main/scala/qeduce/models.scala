ba4afa68b90ce474427261b4719b9af52ee7ae1e
Arnold deVos <adv@bgsig.com>
2021-07-01 20:56:32 +1000

    graft on a TSV parser

--- qeduce/src/main/scala/qeduce/models.scala
+++ qeduce/src/main/scala/qeduce/models.scala
@@ -13,6 +13,8 @@ def declare[R](using t: SQLModel[R])(using Connection): Int = t.declare()()
 def select[R](label: String)(using t: SQLModel[R]): QueryResult[R] = t.select(label)
 def selectAll[R](using t: SQLModel[R]): QueryResult[R] = t.selectAll()
 def insertAll[R](rs: Iterable[R])(using t: SQLModel[R])(using Connection): Int = t.insertAll(rs)()
+def parse[R](label: String, fields: IndexedSeq[String])(using t: SQLModel[R]) = t.parse(label, fields)
+def parseTSV[R](label: String, line: String)(using SQLModel[R]) = parse(label, line.split('\t').toIndexedSeq)
 
 trait SQLModel[R]:
   def insert(r: R): QueryResult[Int]
@@ -20,6 +22,7 @@ trait SQLModel[R]:
   def select(label: String): QueryResult[R]
   def selectAll(): QueryResult[R]
   def insertAll(rs: Iterable[R]): QueryResult[Int]
+  def parse(label: String, fields: IndexedSeq[String]): Option[R]
 
 object SQLModel:
 
@@ -46,6 +49,16 @@ object SQLModel:
 
   def selectTemplate[B](label: String, elements: IndexedSeq[Element[B, SQLType]]): Query =
     sql"select" ~ colNames(elements) ~ sql"from" ~ Query(label)
+
+  def parseFields[B](fields: IndexedSeq[String], elements: IndexedSeq[Element[B, SQLType]], ctor: Product => B): Option[B] =
+    val vs: IndexedSeq[Any] =
+      for 
+        (e, f) <- elements.zip(fields)
+        v <- e.typeclass.parse(f)
+      yield  v
+    if vs.size != elements.size then None
+    else Some(ctor(SeqProduct(vs)))
+
 
   def repeatedInsert[B](bs: Iterable[B], label: String, elements: IndexedSeq[Element[B, SQLType]]): QueryResult[Int] =
     if bs.isEmpty then QueryResult.empty
@@ -85,6 +98,9 @@ object SQLModel:
         select(prod.label)
       def insertAll(rs: Iterable[R]): QueryResult[Int] = 
         repeatedInsert(rs, prod.label, prod.elements)
+      def parse(label: String, fields: IndexedSeq[String]): Option[R] =
+        parseFields(fields, prod.elements, prod.constructor)
+          
 
   def sqlModelForSum[R](sum: SumType[R, SQLType]) = 
     new SQLModel[R]:
@@ -124,12 +140,20 @@ object SQLModel:
             repeatedInsert[b.B](vs, b.label, b.elements)
 
         QueryResult.concat(qrs)
+      def parse(label: String, fields: IndexedSeq[String]): Option[R] =
+        val b = sum.branches.find(_.label == label).get
+        parseFields(fields, b.elements, b.constructor).map(b.upcast)
 
 class RowProduct[B](row: Row, elements: IndexedSeq[Element[B, SQLType]]) extends Product:
   def productElement(ix: Int): Any = 
     val e = elements(ix)
     row(e.label)(using e.typeclass)
   def productArity: Int = elements.size
+  def canEqual(other: Any) = false
+
+class SeqProduct(seq: IndexedSeq[Any]) extends Product:
+  def productElement(ix: Int): Any = seq(ix)
+  def productArity: Int = seq.size
   def canEqual(other: Any) = false
 
 extension(rs: QueryResult[Row])
