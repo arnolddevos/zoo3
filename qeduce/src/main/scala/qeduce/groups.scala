47a5e65a5924472476c543330e0dc72f801860e9
Arnold deVos <adv@bgsig.com>
2021-07-14 19:19:29 +1000

    introduce values and valuenodes

--- qeduce/src/main/scala/qeduce/groups.scala
+++ qeduce/src/main/scala/qeduce/groups.scala
@@ -20,11 +20,14 @@ enum Var[R]:
   case Stored(table: Name, struct: Struct[R])
   case Resident(buffer: ArrayBuffer[R])
 
+enum Value[R]:
+  case QueryValue(query: Query, struct: Struct[R])
+  case GeneratorValue(generator: Connection => Generator[R])
+
 enum Expr[R]:
   case RefNode(subject: Var[R])
-  case QueryNode(query: SelectTemplate, struct: Struct[R])
-  case GenerateNode(generator: Connection => Generator[R])
-  case AggregateNode[R, S, T](group: Group[R, S], aggregate: (S, Connection ?=> Generator[R]) => T) extends Expr[T]
+  case ValueNode(value: Value[R])
+  case AggregateNode[R, S, T](group: Group[R, S], aggregate: (S, Value[R]) => T) extends Expr[T]
   case CountNode[R, S, T](group: Group[R, S], aggregate: (S, Int) => T) extends Expr[T]
   case ProjectNode[R, S](group: Group[R, S]) extends Expr[S]
   case DistinctNode(expr: Expr[R])
@@ -38,46 +41,71 @@ enum Rule:
 
 case class Program( rules: ArraySeq[Rule])
 
-import Var._, Expr._, Group._, Rule._
+import Var._, Value._, Expr._, Group._, Rule._
 
-def interpret[R](e: Expr[R]): Connection ?=> Generator[R] =
+def suspend[R](effect: Connection ?=> Generator[R]): Value[R] =
+  GeneratorValue(connection => effect(using connection))
+
+def release[R](value: Value[R]): Connection ?=> Generator[R] =
+  value match
+    case GeneratorValue(generator) => generator(summon[Connection])
+    case QueryValue(query, struct) => sqlTable(struct).select((_, _) => query)
+
+def interpret[R](e: Expr[R]): Value[R] =
   e match
-    case RefNode(Stored(table, struct)) => sqlTable(struct).select(allRows)
-    case RefNode(Resident(buffer)) => Generator.from(buffer)
-    case QueryNode(query, struct) => sqlTable(struct).select(query)
-    case GenerateNode(generator) => generator(summon[Connection])
+    case RefNode(Stored(table, struct)) => 
+      suspend(
+        sqlTable(struct).select(
+          (cols, _) => allRows(cols, Query(table))
+        )
+      )
+    case RefNode(Resident(buffer)) => suspend(Generator.from(buffer))
+    case ValueNode(value) => value
     case AggregateNode(group, aggregate) => 
-      for (s, g) <- interpret(group) 
-      yield aggregate(s, g)
+      suspend(
+        for (s, g) <- release(interpret(group) )
+        yield aggregate(s, g)
+      )
     case CountNode(group, aggregate) => 
-      for (s, g) <- interpret(group)
-      yield aggregate(s, g.foldLeft(0)((n, _) => n+1))
+      suspend(
+        for (s, g) <- release(interpret(group))
+        yield aggregate(s, release(g).foldLeft(0)((n, _) => n+1))
+      )
     case ProjectNode(group) =>
-      for (s, _) <- interpret(group)
-      yield s
-    case DistinctNode(expr) => Generator.from(interpret(expr).toBuffer.distinct)
+      suspend(
+        for (s, _) <- release(interpret(group))
+        yield s
+      )
+    case DistinctNode(expr) => 
+      suspend(
+        Generator.from(release(interpret(expr)).toBuffer.distinct)
+      )
 
-def interpret[R, S](g: Group[R, S]): Connection ?=> Generator[(S, Connection ?=> Generator[R])] =
+def interpret[R, S](g: Group[R, S]): Value[(S, Value[R])] =
   g match
     case Projection(expr, project, struct) => 
-      val a = interpret(expr).toBuffer.groupBy(project(_))
-      val b = a.map[(S, Connection ?=> Generator[R])]((s, rs) => (s, Generator.from(rs))).toBuffer
-      Generator.from(b)
+      suspend {
+        val a = release(interpret(expr)).toBuffer.groupBy(project(_))
+        val b = a.map((s, rs) => (s, suspend(Generator.from(rs))))
+        Generator.from(b.toBuffer)
+      }
 
     case Mapping(expr, map) => 
-      val a = interpret(expr).toBuffer.groupBy(map)
-      val b = a.map[(S, Connection ?=> Generator[R])]((s, rs) => (s, Generator.from(rs))).toBuffer
-      Generator.from(b)
+      suspend {
+        val a = release(interpret(expr)).toBuffer.groupBy(map)
+        val b = a.map((s, rs) => (s, suspend(Generator.from(rs))))
+        Generator.from(b.toBuffer)
+      }
 
 def interpret(p: Program): Connection ?=> Unit =
   for r <- p.rules
   do
     r match
       case Imply(Stored(table, struct), expr) => 
-        for r <- interpret(expr)
+        for r <- release(interpret(expr))
         do sqlTable(struct).insert(r)
       case Imply(Resident(buffer), expr) => 
-        for r <- interpret(expr)
+        for r <- release(interpret(expr))
         do buffer += r
 
 def sqlTable[R](struct: Struct[R]): SQLTable[R] =
@@ -99,11 +127,11 @@ extension[R](expr: Expr[R])
 
 extension[R, S](group: Group[R, S])
   def count[T](f: (S, Int) => T) = CountNode(group, f)
-  def apply[T](f: (S, Connection ?=> Generator[R]) => T) = AggregateNode(group, f)
+  def apply[T](f: (S, Connection ?=> Generator[R]) => T) = AggregateNode(group, (s, v) => f(s, release(v)))
 
 def select[R](subject: Var[R]) = RefNode(subject)
 def select[X, R](group: Group[X, R]) = ProjectNode(group)
-def select[R](generator: Connection ?=> Generator[R]) = GenerateNode(c => generator(using c))
-def select[R](values: Iterable[R]) = GenerateNode(_ => Generator.from(values))
-def select[R](using struct: Struct[R])(query: SelectTemplate) = QueryNode(query, struct)
+def select[R](generator: Connection ?=> Generator[R]) = ValueNode(suspend(generator))
+def select[R](values: Iterable[R]) = ValueNode(suspend(Generator.from(values)))
+def select[R](using struct: Struct[R])(query: Query) = ValueNode(QueryValue(query, struct))
 def distinct[R](expr: Expr[R]) = DistinctNode(expr)
