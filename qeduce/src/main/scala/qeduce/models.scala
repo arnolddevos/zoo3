5a52365c1085717ec15bdd588c3c0752b9acb45e
Arnold deVos <adv@bgsig.com>
2021-07-23 10:08:11 +1000

    type aliases don't work for type classes

--- qeduce/src/main/scala/qeduce/models.scala
+++ qeduce/src/main/scala/qeduce/models.scala
@@ -9,68 +9,75 @@ import summit.{SumType, ProductType}
 import java.sql.Connection
 import geny.Generator
 import qeduce.helpers._
+import scala.CanEqual.derived
 
 export qeduce.helpers.{SelectTemplate, allRows}
 
-type SQLTable[R] = ProductType[R, SQLType]
-type SQLModel[R] = SumType[R, SQLType]
+trait SQLCapable[R]
+sealed case class SQLTable[R](prod: ProductType[R, SQLType]) extends SQLCapable[R]
+sealed case class SQLModel[R](sum: SumType[R, SQLType]) extends SQLCapable[R]
 
-def tableName[R](using prod: SQLTable[R]) = Query(prod.label)
+object SQLTable:
+  def derived[R](using prod: ProductType[R, SQLType]): SQLTable[R] = SQLTable(prod)
 
-def attribNames[R](using prod: SQLTable[R]): Query = colNames(prod.elements)
+object SQLModel:
+  def derived[R](using sum: SumType[R, SQLType]): SQLModel[R] = SQLModel(sum)
 
-def attribValues[R](r: R)(using prod: SQLTable[R]): Query = colValues(r, prod.elements)
+def tableName[R](using t: SQLTable[R]) = Query(t.prod.label)
 
-def declare[R](using prod: SQLTable[R]): Connection ?=> Int = 
-  createTemplate(prod.label, prod.elements).execute()
+def attribNames[R](using t: SQLTable[R]): Query = colNames(t.prod.elements)
 
-def insert[R](r: R)(using prod: SQLTable[R]): Connection ?=> Int = 
-  insertTemplate(r, prod.label, prod.elements).execute()
+def attribValues[R](r: R)(using t: SQLTable[R]): Query = colValues(r, t.prod.elements)
 
-def insertAll[R](rs: Iterable[R])(using prod: SQLTable[R]): Connection ?=> Int = 
-  repeatedInsert(rs, prod.label, prod.elements)
+def declare[R](using c: SQLCapable[R]): Connection ?=> Int = 
+  c match
+    case SQLTable(prod) => createTemplate(prod.label, prod.elements).execute()
+    case SQLModel(sum) =>
+      var n = 0
+      for b <- sum.branches
+      do n += createTemplate(b.label, b.elements).execute()
+      n
 
-def select[R](using prod: SQLTable[R]): Connection ?=> Generator[R] = select(allRows)(using prod)
+def insert[R](r: R)(using c: SQLCapable[R]): Connection ?=> Int = 
+  c match
+    case SQLTable(prod) => insertTemplate(r, prod.label, prod.elements).execute()
+    case SQLModel(sum) =>
+      val bv = sum.cast(r)
+      insertTemplate(bv.value, bv.branch.label, bv.branch.elements).execute()
 
-def select[R](template: SelectTemplate)(using prod: SQLTable[R]): Connection ?=> Generator[R] =
-  for r <- expandSelect(prod.label, prod.elements, template).results
-  yield prod.constructor(RowProduct(r, prod.elements))
+def insertAll[R](rs: Iterable[R])(using c: SQLCapable[R]): Connection ?=> Int = 
+  c match
+    case SQLTable(prod) => repeatedInsert(rs, prod.label, prod.elements)
+    case SQLModel(sum) =>
+      var n = 0
+      val grps = rs.map(sum.cast(_)).groupBy(_.branch.ordinal)
+      for (_, bvs) <- grps
+      do
+        val b = bvs.head.branch
+        val vs = for bv <- bvs yield bv.value.asInstanceOf[b.B]
+        n += repeatedInsert[b.B](vs, b.label, b.elements)
+      n
 
-def declare[R](using sum: SQLModel[R]): Connection ?=> Int = 
-  var n = 0
-  for b <- sum.branches
-  do n += createTemplate(b.label, b.elements).execute()
-  n
+def select[R](using c: SQLCapable[R]): Connection ?=> Generator[R] = select(allRows)(using c)
 
-def insert[R](r: R)(using sum: SQLModel[R]): Connection ?=> Int = 
-  val bv = sum.cast(r)
-  insertTemplate(bv.value, bv.branch.label, bv.branch.elements).execute()
-  
-def insertAll[R](rs: Iterable[R])(using sum: SQLModel[R]): Connection ?=> Int = 
-  var n = 0
-  val grps = rs.map(sum.cast(_)).groupBy(_.branch.ordinal)
-  for (_, bvs) <- grps
-  do
-    val b = bvs.head.branch
-    val vs = for bv <- bvs yield bv.value.asInstanceOf[b.B]
-    n += repeatedInsert[b.B](vs, b.label, b.elements)
-  n
+def select[R](template: SelectTemplate)(using c: SQLCapable[R]): Connection ?=> Generator[R] =
+  c match
+    case SQLTable(prod) =>
+      for r <- expandSelect(prod.label, prod.elements, template).results
+      yield prod.constructor(RowProduct(r, prod.elements))
+    case SQLModel(sum) =>
+      for
+        b <- Generator.from(sum.branches)
+        q = expandSelect(b.label, b.elements, template)
+        r <- q.results
+      yield
+        b.upcast(b.constructor(RowProduct(r, b.elements)))
 
-def select[R](using sum: SQLModel[R]): Connection ?=> Generator[R] = select(allRows)(using sum)
-
-def select[R](template: SelectTemplate)(using sum: SQLModel[R]): Connection ?=> Generator[R] = 
-  for
-    b <- Generator.from(sum.branches)
-    q = expandSelect(b.label, b.elements, template)
-    r <- q.results
-  yield
-    b.upcast(b.constructor(RowProduct(r, b.elements)))
-
-def selectBranch[R](label: String, template: SelectTemplate = allRows)(using sum: SQLModel[R]): Connection ?=> Generator[R] = 
-  val b = sum.branches.find(_.label == label).get
+def selectBranch[R](label: String, template: SelectTemplate = allRows)(using t: SQLModel[R]): Connection ?=> Generator[R] = 
+  val b = t.sum.branches.find(_.label == label).get
   val q = expandSelect(label, b.elements, template)
   q.results.map(r => b.upcast(b.constructor(RowProduct(r, b.elements))))
 
 extension(rs: Connection ?=> Generator[Row])
   def as[R](using t: SQLTable[R]): Connection ?=> Generator[R] = 
-    rs.map(r => t.constructor(RowProduct(r, t.elements)))
+    rs.map(r => t.prod.constructor(RowProduct(r, t.prod.elements)))
